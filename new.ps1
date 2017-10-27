d5d5326e6bee54df0fa5d2ba97ad260de63e8f43
SovsemKirill <kiriksoap@yandex.ru>
2017-10-27 17:50:19 +0300

    Add delete fails

--- new.ps1
+++ new.ps1
@@ -1,6 +1,7 @@
 if((Get-ChildItem C:\test).length -gt 3)
 {
-	get-ChildItem C:\test | sort LastWriteTime -Descending
+	$count = (get-ChildItem C:\test).length
+	get-ChildItem C:\test | sort LastWriteTime -Descending | select-Object -last(&count-3)|Remove-item
     write-Host to exit press enter
     Read-Host
     Write-Host
