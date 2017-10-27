f0a456dd28b91af20d53b374ec3efd9252fa79e0
SovsemKirill <kiriksoap@yandex.ru>
2017-10-27 20:06:42 +0300

    Final version

--- new.ps1
+++ new.ps1
@@ -1,8 +1,19 @@
-if((Get-ChildItem C:\test).length -gt 3)
+foreach($f1 in Get-ChildItem C:\testLoop | select FullName)
 {
-	$count = (get-ChildItem C:\test).length
-	get-ChildItem C:\test | sort LastWriteTime -Descending | select-Object -last(&count-3)|Remove-item
-    write-Host to exit press enter
-    Read-Host
-    Write-Host
+    $f1 = [string]$f1
+    $f1 = $f1.TrimStart("@{FullName=")
+    $f1 = $f1.TrimEnd("}")    
+        foreach($f2 in Get-ChildItem $f1 | select FullName)
+        {
+            $f2 = [string]$f2
+            $f2 = $f2.TrimStart("@{FullName=")
+            $f2 = $f2.TrimEnd("}")
+                if((Get-ChildItem $f2).length -gt 3)
+                {
+	               get-ChildItem $f2 | sort LastWriteTime -Descending | select-Object -last(((get-ChildItem $f2).length)-3)|Remove-item
+                }
+        }
 }
+write-Host to exit press enter
+Read-Host
+Write-Host
